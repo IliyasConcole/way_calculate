555851f6c78d3a8755d61627e1a76debb2288ab6
Iliyas_Console <iliaskajrakanov@gmail.com>
2025-11-01 14:21:42 +0500

    ver1.1

---
+++ way_speedaa.py
@@ -0,0 +1,14 @@
+while True:
+    if input("Расчитать ускорение ? : (n/y)") == 'y':
+        print("Расчет ускорения")
+        try:
+            t = float(input("введите время в секундах:"))
+            v0 = float(input("введите изначальную скорость в м/с:"))
+            a = float(input("Введите ускорение в м/с²:"))
+            s = (v0 + t ) + (a * t**2) / 2
+            print("Расстояние:", s)
+        except ValueError:
+            print("Неправильное значение")
+    else:
+        print("Пока")
+        break
